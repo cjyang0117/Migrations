7bac0d34407da04040f6f9d0d32096139fdf13ff
harmonics40 <harmonics41@gmail.com>
2020-07-24 14:51:12 +0800

    init

---
+++ Migrations/Data/BlogContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Migrations.Data
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
---
+++ Migrations/Data/BlogContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Migrations.Data
+{
+    public class BlogContextFactory : IDesignTimeDbContextFactory<BlogContext>
+    {
+        public BlogContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BlogContext>();
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\Toturial;Database=BlogDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new BlogContext(optionsBuilder.Options);
+        }
+    }
+}
